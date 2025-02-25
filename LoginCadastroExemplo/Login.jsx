d44607c6d00c6885390d92e26f438c5a0f741cd7
Mateus Scandolera de Sousa <87371774+MateusScandolera21@users.noreply.github.com>
2025-02-24 21:48:57 -0300

    Update Login
    Função para ver a página inicial apenas se fizer login

---
+++ LoginCadastroExemplo/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginSenha, setLoginSenha] = useState('');
+  const [mensagem, setMensagem] = useState('');
+  const navigate = useNavigate();
+
+  // Função para fazer login
+  const fazerLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/login', {
+        email: loginEmail,
+        senha: loginSenha,
+      });
+      setMensagem(`Login bem-sucedido: ${response.data.usuario.email}`);
+      navigate('/Inicio'); // Redireciona para a página de dashboard (ou outra página)
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setMensagem(error.response.data.message); // Exibe a mensagem de erro do backend
+      } else {
+        setMensagem('Erro ao fazer login');
+      }
+    }
+  };
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Login</h1>
+
+      <div>
+        <input
+          type="email"
+          placeholder="Email"
+          value={loginEmail}
+          onChange={(e) => setLoginEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={loginSenha}
+          onChange={(e) => setLoginSenha(e.target.value)}
+        />
+        <button onClick={fazerLogin}>Login</button>
+        {/* <button onClick={fazerCadastro}>Login</button> */}
+      </div>
+
+      {mensagem && <p>{mensagem}</p>}
+    </div>
+  );
+}
+
+export default Login;
